6df0b8cdeaf58c3b1b591dc238ac30da07a65591
HAP96 <HAP96@proton.me>
2023-03-15 18:39:27 -0700

    api setup

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 // b063de98f432135a9d33ed7a5d25fa86
 // https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
-let city = 'Phoenix';
-let APIKey = 'b063de98f432135a9d33ed7a5d25fa86';
-
 async function getWeather() {
+  let city = document.getElementById('city').value;
+  let APIKey = 'b063de98f432135a9d33ed7a5d25fa86';
+  let units = 'imperial';
+
   const weather = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${APIKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${APIKey}`
   );
   const data = await weather.json();
-  console.log(data);
+
+  const temp = document.querySelector('.temperature');
+  temp.textContent = `${data.main.temp.toFixed()}°F`;
+
+  const description = document.querySelector('.description');
+  description.textContent = data.weather[0].description;
+
+  const humidity = document.querySelector('.humidity-value');
+  humidity.textContent = data.main.humidity;
+
+  const wind = document.querySelector('.wind-value');
+  wind.textContent = `${data.wind.speed.toFixed()} mph`;
+
+  const img = document.getElementById('weather-img');
+  const main = data.weather[0].main;
+  if (main === 'Thunderstorm' || main === 'Drizzle' || main === 'Rain') {
+    img.src = 'images/rain.png';
+  } else if (main === 'Snow') {
+    img.src = 'images/snow.png';
+  } else if (main === 'Clear') {
+    img.src = 'images/clear.png';
+  } else if (main === 'Clouds') {
+    img.src = 'images/cloud.png';
+  } else {
+    img.src = 'images/mist.png';
+  }
 }
 
-getWeather();
-
-const pic = document.getElementById('weather-img');
-pic.src = 'images/clear.png';
+const btn = document.getElementById('btn');
+btn.onclick = getWeather;
